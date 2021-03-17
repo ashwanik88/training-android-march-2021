cb26228e679c2ab486ecc6ae1cf3ad5f2d3c5a44
Ashwani <ashwani.k88@gmail.com>
2021-03-17 19:41:29 +0530

    external style & intent redirection

--- app/src/main/java/com/apxic/myapplication/Dashboard.java
+++ app/src/main/java/com/apxic/myapplication/Dashboard.java
@@ -2,7 +2,10 @@ package com.apxic.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button btnAddNumbers = findViewById(R.id.btnAddNumbers);
+
+        btnAddNumbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish(); //return to the first layout
+            }
+        });
+
     }
 }
